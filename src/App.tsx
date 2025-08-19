8257a8986f01562e800fb0fb2f520f7bc0619c32
leandroyyy <leandro.cavallari123@gmail.com>
2025-08-19 20:41:10 -0300

    feat: created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { Header } from "./components/header";
+
 function App() {
   return (
     <>
-      <h1>Revisão cp1</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique in
-        magnam mollitia esse quidem dolorem modi eveniet ut, sunt cumque impedit
-        non, nobis molestias ullam debitis quis nesciunt aut recusandae.
-      </p>
+      <Header />
     </>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Seja bem vindo</h1>
+      <h2>A melhor revisão de cp</h2>
+    </header>
+  );
+}
